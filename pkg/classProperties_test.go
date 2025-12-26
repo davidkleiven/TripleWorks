ae5ce0e5b5be8b17417de79d4ef93403a52d953d
David Kleiven <davidkleiven446@gmail.com>
2025-12-26 19:37:05 +0100

    feat: add functionality to generate golang datamodels

--- pkg/classProperties_test.go
+++ pkg/classProperties_test.go
@@ -1,6 +1,8 @@
 package pkg
 
 import (
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,4 +25,27 @@ func TestClasses(t *testing.T) {
 		num++
 	}
 	assert.Equal(t, num, 1)
+}
+
+func TestWriteBunModel(t *testing.T) {
+	eqGraph := equipmentRdfsGraph()
+	properties := eqGraph.Properties()
+
+	f, err := os.CreateTemp("", "models*.go")
+	if err != nil {
+		t.Fatal("Could not create temp file")
+	}
+	defer f.Close()
+	//defer os.Remove(f.Name())
+
+	types := eqGraph.GolangTypes()
+	properties.WriteAllBunModels(f, *NewTypes(types), "")
+
+	cmd := exec.Command("go", "build", f.Name())
+	err = cmd.Run()
+	assert.Nil(t, err)
+}
+
+func TestCapitalizeFirstEmptyString(t *testing.T) {
+	assert.Equal(t, capitalizeFirst(""), "")
 }
--- pkg/rdfs.go
+++ pkg/rdfs.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"fmt"
 	"io"
+	"strings"
 
 	"gonum.org/v1/gonum/graph/formats/rdf"
 )
@@ -12,9 +13,10 @@ import (
 var resource embed.FS
 
 const (
-	Rdf   = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-	Rdfs  = "http://www.w3.org/2000/01/rdf-schema#"
-	Cim16 = "http://iec.ch/TC57/2013/CIM-schema-cim16#"
+	Rdf     = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	Rdfs    = "http://www.w3.org/2000/01/rdf-schema#"
+	RdfsExt = "http://iec.ch/TC57/1999/rdf-schema-extensions-19990926#"
+	Cim16   = "http://iec.ch/TC57/2013/CIM-schema-cim16#"
 )
 
 // Loads an rdf resource and groups statements by subject
@@ -53,14 +55,33 @@ func (r *RdfsGraph) IsClass(term rdf.Term) bool {
 	return false
 }
 
+func (r *RdfsGraph) UnusedAssociations() map[rdf.Term]struct{} {
+	associationUsed := Must(rdf.NewIRITerm(RdfsExt + "AssociationUsed")).Value
+	iter := r.Graph.AllStatements()
+	skip := make(map[rdf.Term]struct{})
+	for iter.Next() {
+		statement := iter.Statement()
+		if statement.Predicate.Value == associationUsed && strings.Contains(strings.ToLower(statement.Object.Value), "no") {
+			skip[statement.Subject] = struct{}{}
+		}
+	}
+	return skip
+}
+
 // Extract all subjects having 'term' as their domain
 func (r *RdfsGraph) Properties() *PropertyList {
 	properties := NewPropertyList()
 	rdfDomain := Must(rdf.NewIRITerm(Rdfs + "domain")).Value
 	subClassOf := Must(rdf.NewIRITerm(Rdfs + "subClassOf")).Value
+
+	skip := r.UnusedAssociations()
 	iter := r.Graph.AllStatements()
 	for iter.Next() {
 		statement := iter.Statement()
+		_, shouldSkip := skip[statement.Subject]
+		if shouldSkip {
+			continue
+		}
 		switch statement.Predicate.Value {
 		case rdfDomain:
 			properties.AddProperty(statement.Object, statement.Subject)
@@ -69,4 +90,64 @@ func (r *RdfsGraph) Properties() *PropertyList {
 		}
 	}
 	return properties
+}
+
+// Extract the closes golang type for all properties
+func (r *RdfsGraph) GolangTypes() map[string]string {
+	iter := r.Graph.AllStatements()
+	dataType := Must(rdf.NewIRITerm(RdfsExt + "dataType")).Value
+	rdfDomain := Must(rdf.NewIRITerm(Rdfs + "domain")).Value
+	rdfDtype := make(map[rdf.Term]rdf.Term)
+	domains := make(map[rdf.Term][]rdf.Term)
+	for iter.Next() {
+		statement := iter.Statement()
+		switch statement.Predicate.Value {
+		case rdfDomain:
+			// Voltage.value <domain> Voltage
+			// Used to traverse multiple levels of voltages
+			v, ok := domains[statement.Object]
+			if !ok {
+				v = []rdf.Term{statement.Subject}
+			} else {
+				v = append(v, statement.Subject)
+			}
+			domains[statement.Object] = v
+		case dataType:
+			// <subject> dataType Voltage
+			rdfDtype[statement.Subject] = statement.Object
+		}
+	}
+
+	golangTypes := make(map[string]string)
+	for subj, dtype := range rdfDtype {
+		currentType := dtype
+		for {
+			alternatives, ok := domains[currentType]
+			if ok {
+				for _, candidate := range alternatives {
+					newType, ok := rdfDtype[candidate]
+					if ok {
+						currentType = newType
+						break
+					}
+				}
+			} else {
+				break
+			}
+		}
+
+		var goDtype string
+		rdfType := strings.ToLower(currentType.Value)
+		if strings.Contains(rdfType, "float") {
+			goDtype = "float64"
+		} else if strings.Contains(rdfType, "int") {
+			goDtype = "int"
+		} else if strings.Contains(rdfType, "bool") {
+			goDtype = "bool"
+		} else {
+			goDtype = "string"
+		}
+		golangTypes[subj.Value] = goDtype
+	}
+	return golangTypes
 }
