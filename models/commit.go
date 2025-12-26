189637eb49caa53e3f06782c642fd24b80e15db5
David Kleiven <davidkleiven446@gmail.com>
2025-12-26 19:36:47 +0100

    feat: add base model

--- models/commit.go
+++ models/commit.go
@@ -14,3 +14,18 @@ type Commit struct {
 	Author        string    `bun:"author"`
 	CreatedAt     time.Time `bun:"created_at,default:current_timestamp"`
 }
+
+type Model struct {
+	bun.BaseModel `bun:"table:models"`
+	Id            int    `bun:"id,pk,autoincrement"`
+	Name          string `bun:"name"`
+}
+
+type BaseEntity struct {
+	Id       int64 `bun:"id,pk,autoincrement"`
+	ModelId  int64 `bun:"model_id"`
+	CommitId int64 `bun:"commit_id"`
+
+	Commit *Commit `bun:"rel:belongs-to,join:commit_id=id"`
+	Model  *Model  `bun:"rel:belongs-to,join:model_id=id"`
+}
