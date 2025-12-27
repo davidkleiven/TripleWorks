8f035d9f93ffdfe84129bdab24f049aac63daeb4
David Kleiven <davidkleiven446@gmail.com>
2025-12-27 09:13:00 +0100

    feat: add postgres tests to CI pipeline

--- go.mod
+++ go.mod
@@ -14,13 +14,20 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/uptrace/bun/dialect/pgdialect v1.2.16 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- migrations/migrate_test.go
+++ migrations/migrate_test.go
@@ -3,12 +3,16 @@ package migrations
 import (
 	"context"
 	"database/sql"
+	"os"
 	"testing"
+	"time"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/dialect/sqlitedialect"
 	"github.com/uptrace/bun/migrate"
 )
@@ -19,24 +23,68 @@ func setupSqliteTestDb(t *testing.T) *bun.DB {
 	return bun.NewDB(sqldb, sqlitedialect.New())
 }
 
+func setupPostgresTestDb(t *testing.T) *bun.DB {
+	sqldb, err := sql.Open("pgx", pgTestUrl())
+	require.NoError(t, err)
+	return bun.NewDB(sqldb, pgdialect.New())
+}
+
+func pgTestUrl() string {
+	url, ok := os.LookupEnv("POSTGRES_TESTDB")
+	if ok {
+		return url
+	}
+	return "postgres://test:test@localhost:5432/testdb?sslmode=disable"
+}
+
+func skipLocallyIfNoConnection(t *testing.T) {
+	_, isCi := os.LookupEnv("CI")
+	sqldb, err := sql.Open("pgx", pgTestUrl())
+	require.NoError(t, err)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	err = sqldb.PingContext(ctx)
+	if err != nil && isCi {
+		t.Fatalf("Could not context postgres db: %s", err)
+	} else if err != nil {
+		t.Skip("Could not contact postgres db")
+	}
+}
+
 func TestAllMigrations(t *testing.T) {
-	ctx := context.Background()
+	for _, test := range []struct {
+		db   *bun.DB
+		name string
+	}{
+		{
+			db:   setupSqliteTestDb(t),
+			name: "Sqlite",
+		},
+		{
+			db:   setupPostgresTestDb(t),
+			name: "Postgres",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			if test.name == "Postgres" {
+				skipLocallyIfNoConnection(t)
+			}
+			ctx := context.Background()
+			applied, err := RunUp(ctx, test.db)
+			t.Logf("Applied migrations: %d", len(applied.Migrations))
 
-	sqldb, err := sql.Open("sqlite3", "file::memory:?cache-shared")
-	assert.Nil(t, err)
+			assert.Nil(t, err)
 
-	db := bun.NewDB(sqldb, sqlitedialect.New())
-	applied, err := RunUp(ctx, db)
-	t.Logf("Applied migrations: %d", len(applied.Migrations))
+			migrator := migrate.NewMigrator(test.db, migrations)
 
-	assert.Nil(t, err)
+			rolledBack, err := migrator.Rollback(ctx)
+			t.Logf("Rolled back %d", len(rolledBack.Migrations))
+			assert.Nil(t, err)
+			assert.Equal(t, len(rolledBack.Migrations), len(applied.Migrations))
+		})
+	}
 
-	migrator := migrate.NewMigrator(db, migrations)
-
-	rolledBack, err := migrator.Rollback(ctx)
-	t.Logf("Rolled back %d", len(rolledBack.Migrations))
-	assert.Nil(t, err)
-	assert.Equal(t, len(rolledBack.Migrations), len(applied.Migrations))
 }
 
 func TestAddCommitTable(t *testing.T) {
