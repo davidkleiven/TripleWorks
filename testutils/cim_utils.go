31f1e4f823f63cfd3577753888e74f0ed4e86e2c
David Kleiven <davidkleiven446@gmail.com>
2026-01-04 10:30:30 +0100

    fix: use the newly inserted ids

--- testutils/cim_utils.go
+++ testutils/cim_utils.go
@@ -38,8 +38,14 @@ func CreateValidTerminal() *ValidTerminal {
 		EntityType:  pkg.StructName(models.BusNameMarker{}),
 	}
 
+	conNode := models.Entity{
+		Mrid:        uuid.New(),
+		ModelEntity: models.ModelEntity{ModelId: 1},
+		EntityType:  pkg.StructName(models.ConnectivityNode{}),
+	}
+
 	return &ValidTerminal{
-		Entities: []models.Entity{terminalEntity, condEquipment, busNameMarker},
+		Entities: []models.Entity{terminalEntity, condEquipment, busNameMarker, conNode},
 		Terminal: &models.Terminal{
 			ConductingEquipmentMrid: condEquipment.Mrid,
 			ACDCTerminal: models.ACDCTerminal{
@@ -47,7 +53,8 @@ func CreateValidTerminal() *ValidTerminal {
 				SequenceNumber:    1,
 				IdentifiedObject:  models.IdentifiedObject{BaseEntity: models.BaseEntity{CommitId: 1}},
 			},
-			PhasesId: 1,
+			PhasesId:           1,
+			TerminalOperations: models.TerminalOperations{ConnectivityNodeMrid: conNode.Mrid},
 		},
 		Model:  &model,
 		Commit: &commit,
@@ -64,11 +71,13 @@ func InsertTerminalFactory(data *ValidTerminal) func(context.Context, bun.Tx) er
 		}
 
 		for i, entity := range data.Entities {
+			entity.ModelId = data.Model.Id
 			if _, err := tx.NewInsert().Model(&entity).Exec(ctx); err != nil {
 				return fmt.Errorf("Failed to insert entity %d: %w", i, err)
 			}
 		}
 
+		data.Terminal.CommitId = int(data.Commit.Id)
 		if _, err := tx.NewInsert().Model(data.Terminal).Exec(ctx); err != nil {
 			return fmt.Errorf("Failed to insert terminal: %w", err)
 		}
