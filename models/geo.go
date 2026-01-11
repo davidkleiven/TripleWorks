4006a199ba74bd29937ab8a0c74420b0e517774f
David Kleiven <davidkleiven446@gmail.com>
2026-01-11 18:53:08 +0100

    wip: add geo location go structs

---
+++ models/geo.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type LocatedPowerSystemResource struct {
+	LocationMrid uuid.UUID `bun:"location_mrid,type:uuid" json:"location_mrid"`
+	Location     *Entity   `bun:"rel:belongs-to,join:location_mrid=mrid" json:"location,omitempty"`
+}
+
+type Location struct {
+	IdentifiedObject
+	CoordinateSystemMrid uuid.UUID `bun:"coordinate_system_mrid,type:uuid" json:"coordinate_system_mrid"`
+	CoordinateSystem     *Entity   `bun:"rel:belongs-to,join:coordinate_system_mrid=mrid" json:"coordinate_system,omitempty"`
+}
+type PositionPoint struct {
+	YPosition      string    `bun:"yposition" json:"yposition" iri:"http://iec.ch/TC57/2013/CIM-schema-cim16#PositionPoint.yPosition"`
+	ZPosition      string    `bun:"zposition" json:"zposition" iri:"http://iec.ch/TC57/2013/CIM-schema-cim16#PositionPoint.zPosition"`
+	SequenceNumber int       `bun:"sequence_number" json:"sequence_number" iri:"http://iec.ch/TC57/2013/CIM-schema-cim16#PositionPoint.sequenceNumber"`
+	XPosition      string    `bun:"xposition" json:"xposition" iri:"http://iec.ch/TC57/2013/CIM-schema-cim16#PositionPoint.xPosition"`
+	LocationMrid   uuid.UUID `bun:"location_mrid,type:uuid" json:"location_mrid"`
+	Location       *Entity   `bun:"rel:belongs-to,join:location_mrid=mrid" json:"location,omitempty"`
+}
+type CoordinateSystem struct {
+	IdentifiedObject
+	CrsUrn string `bun:"crs_urn" json:"crs_urn" iri:"http://iec.ch/TC57/2013/CIM-schema-cim16#CoordinateSystem.crsUrn"`
+}
