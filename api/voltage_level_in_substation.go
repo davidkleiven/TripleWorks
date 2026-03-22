11a743a1f7e4c43017a8883cfaa1dea8c84b224c
David Kleiven <davidkleiven446@gmail.com>
2026-03-22 12:29:48 +0100

    feat: make in voltage level end point support both application/json and text/html responses

--- api/voltage_level_in_substation.go
+++ api/voltage_level_in_substation.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"strings"
 	"time"
 
 	"com.github/davidkleiven/tripleworks/models"
+	"com.github/davidkleiven/tripleworks/pkg"
 	"com.github/davidkleiven/tripleworks/repository"
 )
 
@@ -35,6 +37,13 @@ func (i *InVoltageLevelEndpoint) VoltageLevelsInSubstation(w http.ResponseWriter
 		Mrid:          mrid,
 		VoltageLevels: vls,
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(result)
+
+	switch accept := r.Header.Get("Accept"); {
+	case strings.Contains(accept, pkg.ContentTypeJSON):
+		w.Header().Set(pkg.ContentType, pkg.ContentTypeJSON)
+		json.NewEncoder(w).Encode(result)
+	default:
+		w.Header().Set(pkg.ContentType, pkg.ContentTypeHTML)
+		pkg.CreateList(w, vls)
+	}
 }
