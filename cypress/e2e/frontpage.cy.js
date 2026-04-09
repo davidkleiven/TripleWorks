a0bb2c729e4a78595442a3bc37517350823b5df4
David Kleiven <davidkleiven446@gmail.com>
2026-04-09 18:54:35 +0200

    fix: bug in commit. Model was sent as a double pointer instead of pointer

--- cypress/e2e/frontpage.cy.js
+++ cypress/e2e/frontpage.cy.js
@@ -3,4 +3,18 @@ describe("Load front page", () => {
     cy.visit("/");
     cy.get("h1").should("have.text", "TripleWorks");
   });
+
+  it("can create new item", () => {
+    cy.visit("/");
+    cy.get("#type-select").select("ReportingGroup");
+    cy.get("#new-btn").click();
+    cy.get("#commit-input").type("create an empty reporting group");
+    cy.intercept("POST", "/commit").as("commitReq");
+    cy.get("#commit-btn").click();
+    cy.wait("@commitReq").then((intersception) => {
+      expect(intersception.response.statusCode).to.equal(200);
+    });
+
+    cy.get("#status-message").should("contain", "Successfully updated");
+  });
 });
